f3136bffb390820c3e013015730470f3cdcc223f
Manuel Schiller <manuel.schiller@caligano.de>
2025-07-14 00:53:48 +0200

    add import

--- packages/react-router/tests/Scripts.test.tsx
+++ packages/react-router/tests/Scripts.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test, vi } from 'vitest'
 import { act, render, screen } from '@testing-library/react'
 import ReactDOMServer from 'react-dom/server'
 
